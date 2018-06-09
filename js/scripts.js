9479ecb29d41734ad0ab65f47e91b26d0bc2590d
AphoomZhah <aphzhah@gmail.com>
2018-06-09 19:49:12 -0400

    step by step - complete profile

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,43 @@
-$(document).ready(function () {
+jQuery(document).ready(function ($) {
     // Toggle Menu 
     $('#toggle').click(function () {
        $('#nav-menu').toggleClass('active');
     });
+    
+    //Initialize tooltips
+    $('.nav-tabs > li a[title]').tooltip();
+    
+    //Wizard
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+
+        var $target = $(e.target);
+    
+        if ($target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
+
+    $(".next-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        $active.next().removeClass('disabled');
+        nextTab($active);
+
+    });
+    $(".prev-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        prevTab($active);
+
+    });
 });
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
+}
 
 
 
